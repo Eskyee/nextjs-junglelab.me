d04f7255b74d95188a4ffcd203b5e5d6aee439a9
Esky33 <djescaba@icloud.com>
2022-07-27 02:53:10 +0100

    add metadata

--- utils/global-data.js
+++ utils/global-data.js
@@ -1,13 +1,13 @@
 export const getGlobalData = () => {
   const name = process.env.BLOG_NAME
     ? decodeURI(process.env.BLOG_NAME)
-    : 'Jay Doe';
+    : 'junglelab education';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : 'Next.js Blog Theme';
+    : 'Bitcoin Liquid Eco System';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
-    : 'All rights reserved.';
+    : 'JUNGLELABᵀᴹ.';
 
   return {
     name,
