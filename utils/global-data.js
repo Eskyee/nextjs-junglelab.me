22db12679e310f95a165181f926cbf600f2ea135
Esky33 <djescaba@icloud.com>
2022-09-04 05:37:11 +0100

    update metadata

--- utils/global-data.js
+++ utils/global-data.js
@@ -1,10 +1,10 @@
 export const getGlobalData = () => {
   const name = process.env.BLOG_NAME
     ? decodeURI(process.env.BLOG_NAME)
-    : 'junglelab education';
+    : 'JUNGLELABᵀᴹ';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : 'Bitcoin Liquid Eco System';
+    : 'Eco System';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
     : 'JUNGLELABᵀᴹ.';
